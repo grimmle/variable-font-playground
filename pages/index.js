1a0151dfa31c4e2cc3a6c2e05a38551e7d1ba527
Lennard Grimm <hi@grimmle.com>
2021-10-22 00:54:24 +0200

    revamped state to dynamically render axes sliders

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,48 @@
 import * as React from 'react';
 import { Button, Slider, Container, Box, Select, MenuItem } from '@mui/material';
 import { useState, useEffect } from 'react';
-import fs from 'fs';
 import opentype from 'opentype.js'
+import fs from 'fs';
 
 
 export default function App({ fonts }) {
   const [font, setFont] = useState(fonts[0])
-  const [wght, setWght] = useState(font.axes.find((a) => a.tag ==='wght')?.defaultValue || 500)
-  const [wdth, setWdth] = useState(font.axes.find((a) => a.tag ==='wdth')?.defaultValue || 100)
-  const [cnst, setCnst] = useState(font.axes.find((a) => a.tag ==='cnst')?.defaultValue || 50)
-  const [ital, setItal] = useState(font.axes.find((a) => a.tag ==='ital')?.defaultValue || 5)
-  const [opsz, setOpsz] = useState(font.axes.find((a) => a.tag ==='opsz')?.defaultValue || 50)
-  const [slnt, setSlnt] = useState(font.axes.find((a) => a.tag ==='slnt')?.defaultValue || 25)
-  const [size, setSize] = useState(50)
-  const [letterSpace, setLetterSpace] = useState(0)
-  const [lineHeight, setLineHeight] = useState(1)
-  const [transform, setTransform] = useState('')
-  const [align, setAlignment] = useState('left')
+  const [fontAxes, setFontAxes] = useState(Object.assign({}, ...font.axes.map((axis) => {return {[axis.tag]: axis.defaultValue}})));
+  const [variation, setVariation] = useState('')
+  const [transform, setTransform] = useState({
+    size: 50,
+    letterSpace: 1,
+    lineHeight: 1,
+    transform: '',
+    align: 'left',
+  })
 
-  const changeTransform = (transform) => {
-    setTransform(transform)
-  }
-  const changeAlign = (align) => {
-    setAlignment(align);
-  }
+  useEffect(() => {
+    const ax = Object.assign({}, ...font.axes.map((axis) => {return {[axis.tag]: axis.defaultValue}}))
+    setFontAxes(ax)
+  }, [font])
 
+  useEffect(() => {
+    const v = Object.keys(fontAxes).map((ax) => {
+      return `'${ax}' ${fontAxes[ax]}`
+    }).join(', ')
+    setVariation(v)
+  }, [fontAxes])
+
+  const handleChange = (axis, e) => {
+    if (axis === 'font') setFont(e.target.value);
+    else if (fontAxes[axis] !== undefined) setFontAxes({...fontAxes, [axis]: e.target.value})
+    else setTransform({...transform, [axis]: e.target.value})
+  }
+  
   const style = {
     'fontFamily': font.name,
-    'fontVariationSettings': `'wght' ${wght}, 'wdth' ${wdth}, 'cnst' ${cnst}, 'ital' ${ital}, 'opsz' ${opsz}, 'slnt' ${slnt}`,
-    'textTransform': transform, 
-    'fontSize': size, 
-    'textAlign': align, 
-    'letterSpacing': letterSpace, 
-    'lineHeight': lineHeight 
-  }
-  const handleChange = (axis, e) => {
-    switch(axis) {
-      case 'font': setFont(e.target.value); break;
-      case 'size': setSize(e.target.value); break;
-      case 'wght': setWght(e.target.value); break;
-      case 'wdth': setWdth(e.target.value); break;
-      case 'cnst': setCnst(e.target.value); break;
-      case 'ital': setItal(e.target.value); break;
-      case 'opsz': setOpsz(e.target.value); break;
-      case 'slnt': setSlnt(e.target.value); break;
-      case 'letterSpace': setLetterSpace(e.target.value); break;
-      case 'lineHeight': setLineHeight(e.target.value); break;
-      default: break;
-    }
+    'fontVariationSettings': variation,
+    'textTransform': transform.transform, 
+    'fontSize': transform.size, 
+    'textAlign': transform.align, 
+    'letterSpacing': transform.letterSpace, 
+    'lineHeight': transform.lineHeight
   }
 
   return (
@@ -64,70 +58,36 @@ export default function App({ fonts }) {
 
         <h3>Transform</h3>
         <Box>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeTransform('uppercase')}}>Uppercase</Button>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeTransform('capitalize')}}>Capitalize</Button>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeTransform('lowercase')}}>Lowercase</Button>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeTransform('')}}>None</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="uppercase" onClick={e => {handleChange('transform', e)}}>Uppercase</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="capitalize" onClick={e => {handleChange('transform', e)}}>Capitalize</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="lowercase" onClick={e => {handleChange('transform', e)}}>Lowercase</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="" onClick={e => {handleChange('transform', e)}}>None</Button>
         </Box>
 
         <h3>Align</h3>
         <Box>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeAlign('left')}}>Left</Button>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeAlign('center')}}>Center</Button>
-          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} onClick={e => {changeAlign('right')}}>Right</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="left" onClick={e => {handleChange('align', e)}}>Left</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="center" onClick={e => {handleChange('align', e)}}>Center</Button>
+          <Button variant="outlined" size="small" color="primary" style={{margin: '0 10px'}} value="right" onClick={e => {handleChange('align', e)}}>Right</Button>
         </Box>
 
         <h3>Vary</h3>
         <Box sx={{padding: '10px'}}>
           <p>Size</p>
-          <Slider size="small" value={size} min={10} max={100} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('size', e)} aria-label="Size">Size</Slider>
+          <Slider size="small" value={transform.size} min={10} max={100} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('size', e)} aria-label="Size">Size</Slider>
           <p>Letter Spacing</p>
-          <Slider size="small" value={letterSpace} min={-10} max={50} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('letterSpace', e)} aria-label="Letter Spacing">Letter Spacing</Slider>
+          <Slider size="small" value={transform.letterSpace} min={-10} max={50} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('letterSpace', e)} aria-label="Letter Spacing">Letter Spacing</Slider>
           <p>Line Height</p>
-          <Slider size="small" value={lineHeight} min={0} max={200} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('lineHeight', e)} aria-label="Line Height">lineHeight</Slider>
-          {/* 
-          TODO: find a way to access wght variable from axis.tag
+          <Slider size="small" value={transform.lineHeight} min={0} max={200} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('lineHeight', e)} aria-label="Line Height">lineHeight</Slider>
           {font.axes.map((axis) => {
             const key = [axis.tag]
-            console.log(key)
             return (
-              <>
+              <div key={axis.tag}>
                 <p>{axis.name.en}</p>
-                <Slider size="small" value={[key]} defaultValue={axis.defaultValue} min={axis.minValue} max={axis.maxValue} valueLabelDisplay="auto" onChange={(e) => handleChange(axis.tag, e)} aria-label={axis.name.en}></Slider>
-              </>
+                <Slider size="small" value={fontAxes[key] || 0} defaultValue={axis.defaultValue} min={axis.minValue} max={axis.maxValue} valueLabelDisplay="auto" onChange={(e) => handleChange(axis.tag, e)} aria-label={axis.name.en}></Slider>
+              </div>
             )
-          })} */}
-
-          {font.axes && font.axes.some((a) => a.tag ==='wght') && (
-            <>
-                <p>Weight</p>
-                <Slider size="small" value={wght} defaultValue={font.axes.find((a) => a.tag ==='wght').defaultValue} min={font.axes.find((a) => a.tag === 'wght').minValue} max={font.axes.find((a) => a.tag ==='wght').maxValue} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('wght', e)} aria-label="Weight"></Slider>
-              </>)}
-          {font.axes && font.axes.some((a) => a.tag ==='wdth') && (
-            <>
-              <p>Width</p>
-              <Slider size="small" value={wdth} defaultValue={font.axes.find((a) => a.tag ==='wdth').defaultValue} min={font.axes.find((a) => a.tag === 'wdth').minValue} max={font.axes.find((a) => a.tag ==='wdth').maxValue} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('wdth', e)} aria-label="Width"></Slider>
-            </>)}
-          {font.axes && font.axes.some((a) => a.tag ==='cnst') && (
-            <>
-              <p>Contrast</p>
-              <Slider size="small" value={cnst} defaultValue={font.axes.find((a) => a.tag ==='cnst').defaultValue} min={font.axes.find((a) => a.tag === 'cnst').minValue} max={font.axes.find((a) => a.tag ==='cnst').maxValue} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('cnst', e)} aria-label="Contrast"></Slider>
-            </>)}
-          {font.axes && font.axes.some((a) => a.tag ==='ital') && (
-            <>
-              <p>Italic</p>
-              <Slider size="small" value={ital} defaultValue={font.axes.find((a) => a.tag ==='ital').defaultValue} min={font.axes.find((a) => a.tag === 'ital').minValue} max={font.axes.find((a) => a.tag ==='ital').maxValue} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('ital', e)} aria-label="Italic"></Slider>
-            </>)}
-          {font.axes && font.axes.some((a) => a.tag ==='opsz') && (
-            <>
-              <p>Optical Size</p>
-              <Slider size="small" value={opsz} defaultValue={font.axes.find((a) => a.tag ==='opsz').defaultValue} min={font.axes.find((a) => a.tag === 'opsz').minValue} max={font.axes.find((a) => a.tag ==='opsz').maxValue} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('opsz', e)} aria-label="Optical Size"></Slider>
-            </>)}
-          {font.axes && font.axes.some((a) => a.tag ==='slnt') && (
-            <>
-              <p>Slant</p>
-              <Slider size="small" value={slnt} defaultValue={font.axes.find((a) => a.tag ==='slnt').defaultValue} min={font.axes.find((a) => a.tag === 'slnt').minValue} max={font.axes.find((a) => a.tag ==='slnt').maxValue} valueLabelDisplay="auto" color="primary" onChange={(e) => handleChange('slnt', e)} aria-label="Slant"></Slider>
-            </>)}
+          })}
         </Box>
       </Container>
     </>
