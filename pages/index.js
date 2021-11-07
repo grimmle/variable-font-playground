607dbde1e466927b46aef3057ca56c9e8b38ab9c
Lennard Grimm <hi@grimmle.com>
2021-11-07 21:11:59 +0100

    add preset select and font credits

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import opentype from 'opentype.js';
 import fs from 'fs';
 
 import Label from '../components/label';
+import Credits from '../components/credits';
 import { Button, Slider, Box, Select, MenuItem } from '@mui/material';
 import { updateGradient,  buildFont } from '../lib/helpers';
 import { dropHandler, dragOverHandler, dragLeaveHandler } from '../lib/dropzone';
@@ -15,6 +17,7 @@ export default function App({ localFonts }) {
   const [fonts, setFonts] = useState(localFonts)
   const [font, setFont] = useState(fonts[0])
   const [fontAxes, setFontAxes] = useState({});
+  const [fontInstance, setFontInstance] = useState(font.instances[0] || '');
   const [variation, setVariation] = useState('')
   const [transform, setTransform] = useState({
     size: 50,
@@ -67,6 +70,7 @@ export default function App({ localFonts }) {
     if (!font.axes) return 
     const ax = Object.assign({}, ...font.axes.map((axis) => {return {[axis.tag]: axis.defaultValue}}))
     setFontAxes(ax)
+    setFontInstance(font.instances[0] || '')
   }, [font])
 
   useEffect(() => {
@@ -76,8 +80,18 @@ export default function App({ localFonts }) {
     setVariation(v)
   }, [fontAxes])
 
+  useEffect(() => {
+    if (!fontInstance) return
+    const v = Object.keys(fontInstance.coordinates).map((ax) => {
+      fontAxes[ax] = fontInstance.coordinates[ax]
+      return `'${ax}' ${fontInstance.coordinates[ax]}`
+    }).join(', ')
+    setVariation(v)
+  }, [fontInstance])
+
   const handleChange = (axis, e) => {
     if (axis === 'font') setFont(e.target.value);
+    else if (axis === 'instance') setFontInstance(e.target.value);
     else if (fontAxes[axis] !== undefined) setFontAxes({...fontAxes, [axis]: e.target.value})
     else setTransform({...transform, [axis]: e.target.value})
   }
@@ -113,65 +127,83 @@ export default function App({ localFonts }) {
         </div>
         <div className="container">
           <div className="sidebar">
-            <div style={{background: 'linear-gradient(-45deg, rgb(74, 65, 231) 0%, rgb(233, 33, 109) 100%)', boxShadow: '0px 2px 6px rgba(0,0,0,0.3)', padding: '10px', borderRadius: '25px'}}>
-              <Box sx={{ position: 'relative', background: 'linear-gradient(-45deg, rgb(233, 33, 109) 0%, rgb(74, 65, 231) 100%)', padding: '20px 20px 50px', borderRadius: '20px'}}>
-                <div className="conic-gradient top" />
-                <div className="conic-gradient bottom" />
+          <div style={{background: 'linear-gradient(-45deg, rgb(74, 65, 231) 0%, rgb(233, 33, 109) 100%)', boxShadow: '0px 2px 6px rgba(0,0,0,0.3)', padding: '10px', borderRadius: '25px'}}>
+            <Box sx={{ position: 'relative', background: 'linear-gradient(-45deg, rgb(233, 33, 109) 0%, rgb(74, 65, 231) 100%)', padding: '20px 20px', borderRadius: '20px'}}>
+              <h3>Font</h3>
+              {/* <p className="info">
+                play with these free to use fonts or drag &apos;n drop your own
+              </p> */}
+              <div className='select-container'>
+                <Select value={font} onChange={e => {handleChange('font', e)}} color="primary" variant="standard" sx={{marginLeft: '10px'}}>
+                  {fonts.map((font, id) => <MenuItem key={id} value={font}>{font.name}</MenuItem>)}
+                </Select>
+              </div>
+              <h3>Preset</h3>
+              <div className='select-container'>
+                <Select value={fontInstance} onChange={e => {handleChange('instance', e)}} color="primary" variant="standard" sx={{marginLeft: '10px'}}>
+                  {font.instances
+                    ? (font.instances.map((instance, id) => {
+                      if(!instance.name?.en) return
+                      return <MenuItem key={id} value={instance}>{instance.name.en}</MenuItem>}))
+                    : <MenuItem disabled value="">None</MenuItem>}
+                </Select>
+              </div>
+              <Credits font={font}/>
+            </Box>
+          </div>
 
-                <h3>Font</h3>
-                <div className='button-container'>
-                  <Select value={font} onChange={e => {handleChange('font', e)}} color="primary" variant="outlined" sx={{marginLeft: '10px'}}>
-                    {fonts.map((font, id) => <MenuItem key={id} value={font}>{font.name}</MenuItem>)}
-                  </Select>
-                </div>
+          <div style={{background: 'linear-gradient(-45deg, rgb(74, 65, 231) 0%, rgb(233, 33, 109) 100%)', boxShadow: '0px 2px 6px rgba(0,0,0,0.3)', margin: '20px 0 0 0', padding: '10px', borderRadius: '25px'}}>
+            <Box sx={{ position: 'relative', background: 'linear-gradient(-45deg, rgb(233, 33, 109) 0%, rgb(74, 65, 231) 100%)', padding: '20px 20px 50px', borderRadius: '20px'}}>
+              <div className="conic-gradient top" />
+              <div className="conic-gradient bottom" />
 
-                <h3>Transform</h3>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="uppercase" onClick={e => {handleChange('transform', e)}}>Uppercase</Button>
-                </div>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="lowercase" onClick={e => {handleChange('transform', e)}}>Lowercase</Button>
-                </div>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="capitalize" onClick={e => {handleChange('transform', e)}}>Capitalize</Button>
-                </div>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="" onClick={e => {handleChange('transform', e)}}>None</Button>
-                </div>
+              <h3>Transform</h3>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="uppercase" onClick={e => {handleChange('transform', e)}}>Uppercase</Button>
+              </div>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="lowercase" onClick={e => {handleChange('transform', e)}}>Lowercase</Button>
+              </div>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="capitalize" onClick={e => {handleChange('transform', e)}}>Capitalize</Button>
+              </div>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="" onClick={e => {handleChange('transform', e)}}>None</Button>
+              </div>
 
-                <h3>Align</h3>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="left" onClick={e => {handleChange('align', e)}}>Left</Button>
-                </div>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="center" onClick={e => {handleChange('align', e)}}>Center</Button>
-                </div>
-                <div className='button-container'>
-                  <Button variant="outlined" size="small" color="primary" value="right" onClick={e => {handleChange('align', e)}}>Right</Button>
-                </div>
-              </Box>
-            </div>
+              <h3>Align</h3>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="left" onClick={e => {handleChange('align', e)}}>Left</Button>
+              </div>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="center" onClick={e => {handleChange('align', e)}}>Center</Button>
+              </div>
+              <div className='button-container'>
+                <Button variant="outlined" size="small" color="primary" value="right" onClick={e => {handleChange('align', e)}}>Right</Button>
+              </div>
+            </Box>
+          </div>
 
-            <div style={{background: 'linear-gradient(-220deg,white 0%,lightgray 20%,white 60%,gray 100%)', boxShadow: '0px 2px 6px rgba(0,0,0,0.3)', margin: '20px 0 0 0', padding: '10px', borderRadius: '25px'}}>
-              <Box sx={{padding: '10px', background: 'linear-gradient(-40deg,white 0%,lightgray 20%,white 60%,gray 100%)', padding: '15px', borderRadius: '20px'}}>
-                <h3>Vary</h3>
-                <Label>Size {transform.size}</Label>
-                <Slider size="small" value={transform.size} min={10} max={300} sx={{'& .MuiSlider-track': { background: updateGradient(10, 300, transform.size)}}} valueLabelDisplay="off" color="primary" onChange={(e) => handleChange('size', e)} aria-label="Size"></Slider>
-                <Label>Letter Spacing {transform.letterSpace}</Label>
-                <Slider size="small" value={transform.letterSpace} min={0} max={50} sx={{'& .MuiSlider-track': { background: updateGradient(0, 50, transform.letterSpace)}}} valueLabelDisplay="off" color="primary" onChange={(e) => handleChange('letterSpace', e)} aria-label="Letter Spacing">Letter Spacing</Slider>
-                <Label>Line Height {transform.lineHeight}</Label>
-                <Slider size="small" value={transform.lineHeight} min={0} max={3} step={0.1} sx={{'& .MuiSlider-track': { background: updateGradient(0, 3, transform.lineHeight)}}} valueLabelDisplay="off" color="primary" onChange={(e) => handleChange('lineHeight', e)} aria-label="Line Height">lineHeight</Slider>
-                {font.axes?.map((axis) => {
-                  const key = [axis.tag]
-                  return (
-                    <div key={axis.tag}>
-                      <Label>{axis.name.en} {fontAxes[key]}</Label>
-                      <Slider size="small" value={fontAxes[key] || 0} defaultValue={axis.defaultValue} min={axis.minValue} max={axis.maxValue} sx={{'& .MuiSlider-track': { background: updateGradient(axis.minValue, axis.maxValue, fontAxes[key])}}} valueLabelDisplay="off" onChange={(e) => handleChange(axis.tag, e)} aria-label={axis.name.en}></Slider>
-                    </div>
-                  )
-                })}
-              </Box>
-            </div>
+          <div style={{background: 'linear-gradient(-220deg,white 0%,lightgray 20%,white 60%,gray 100%)', boxShadow: '0px 2px 6px rgba(0,0,0,0.3)', margin: '20px 0 0 0', padding: '10px', borderRadius: '25px'}}>
+            <Box sx={{padding: '10px', background: 'linear-gradient(-40deg,white 0%,lightgray 20%,white 60%,gray 100%)', padding: '15px', borderRadius: '20px'}}>
+              <h3>Vary</h3>
+              <Label>Size {transform.size}</Label>
+              <Slider size="small" value={transform.size} min={10} max={300} sx={{'& .MuiSlider-track': { background: updateGradient(10, 300, transform.size)}}} valueLabelDisplay="off" color="primary" onChange={(e) => handleChange('size', e)} aria-label="Size"></Slider>
+              <Label>Letter Spacing {transform.letterSpace}</Label>
+              <Slider size="small" value={transform.letterSpace} min={0} max={50} sx={{'& .MuiSlider-track': { background: updateGradient(0, 50, transform.letterSpace)}}} valueLabelDisplay="off" color="primary" onChange={(e) => handleChange('letterSpace', e)} aria-label="Letter Spacing">Letter Spacing</Slider>
+              <Label>Line Height {transform.lineHeight}</Label>
+              <Slider size="small" value={transform.lineHeight} min={0} max={3} step={0.1} sx={{'& .MuiSlider-track': { background: updateGradient(0, 3, transform.lineHeight)}}} valueLabelDisplay="off" color="primary" onChange={(e) => handleChange('lineHeight', e)} aria-label="Line Height">lineHeight</Slider>
+              {font.axes?.map((axis) => {
+                const key = [axis.tag]
+                return (
+                  <div key={axis.tag}>
+                    <Label>{axis.name.en} {Math.round(fontAxes[key] * 10) / 10}</Label>
+                    <Slider size="small" value={fontAxes[key] || 0} defaultValue={axis.defaultValue} min={axis.minValue} max={axis.maxValue} sx={{'& .MuiSlider-track': { background: updateGradient(axis.minValue, axis.maxValue, fontAxes[key])}}} valueLabelDisplay="off" onChange={(e) => handleChange(axis.tag, e)} aria-label={axis.name.en}></Slider>
+                  </div>
+                )
+              })}
+            </Box>
+          </div>
         </div>
 
         <textarea id='variable-text-area' style={style} defaultValue='The quick brown fox jumps over the lazy dog'></textarea>
